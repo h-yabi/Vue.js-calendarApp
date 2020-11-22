371ce41655d0091064d568012ac3cdca44980e81
Hiroya Yabiku <h_yabiku@atoj.co.jp>
2020-11-22 14:05:49 +0900

    Vuexインストール

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 import vuetify from './plugins/vuetify';
+import Vuex from 'vuex';
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  Vuex,
   render: h => h(App)
 }).$mount('#app')
