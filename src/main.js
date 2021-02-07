359c753b2e18ce43653a684afc79f8c587a86c04
Hiroya Yabiku <h_yabiku@atoj.co.jp>
2021-02-07 10:32:09 +0900

    Firebaseのライブラリインストール

--- src/main.js
+++ src/main.js
@@ -2,9 +2,22 @@ import Vue from 'vue';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import store from './store';
-import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/css/all.css';
+import firebase from 'firebase';
 
 Vue.config.productionTip = false
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDfnsVF1Q01v39cccwFxeznnofUWRAWVfw",
+  authDomain: "vuejs-calendar-app.firebaseapp.com",
+  projectId: "vuejs-calendar-app",
+  storageBucket: "vuejs-calendar-app.appspot.com",
+  messagingSenderId: "1035539345463",
+  appId: "1:1035539345463:web:150eb56013cb6c52e30fb2"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   vuetify,
